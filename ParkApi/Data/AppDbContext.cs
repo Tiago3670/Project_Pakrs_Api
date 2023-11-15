3758613f05ac55a7cccc0941955b0812cf8b0306
Tiago Almeida <56228294+Tiago3670@users.noreply.github.com>
2023-11-15 17:06:41 +0000

    connection to DB

---
+++ ParkApi/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ParkApi.Data
+{
+    public class AppDbContext :DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) 
+        {
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
